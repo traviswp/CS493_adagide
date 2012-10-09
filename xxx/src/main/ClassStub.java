3158bcd1bd89292caa7dd23c1a1191bd02fba9df
traviswp <traviswp@gmail.com>
2012-10-08 20:52:33 -0700

    Update xxx/src/main/ClassStub.java
    confirmed - must fetch, then merge.

--- xxx/src/main/ClassStub.java
+++ xxx/src/main/ClassStub.java
@@ -8,4 +8,6 @@ public class ClassStub {
 	
 	// the last one worked but I had to fetch, then merge. 
 	// now I am JUST going to merge local & select the tracking branch...
+	
+	// Okay you DEFINITELLY have to FETCH first and then MERGE.
 }
