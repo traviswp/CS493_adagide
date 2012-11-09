252087745a58acbdf91c70e3fbc322eb99303ddb
traviswp <traviswp@gmail.com>
2012-11-09 08:06:47 -0800

    sorry I forgot to commit and push the CompilerLib as well...

--- xxx/src/main/CompilerLib.py
+++ xxx/src/main/CompilerLib.py
@@ -8,99 +8,103 @@ from PyQt4 import QtGui
 
 class ClangCompiler():
 
-    """
-    ----------------------------------------------------------------------
-    This class is designed to provide the ability for the caller to 
-    access the clang compiler. This class supports:
-        + compiling 
-        + determing compiler message types
-        + parsing compiler messages
-    ----------------------------------------------------------------------
-    """
+	"""
+	----------------------------------------------------------------------
+	This class is designed to provide the ability for the caller to 
+	access the clang compiler. This class supports:
+	+ compiling 
+	+ determing compiler message types
+	+ parsing compiler messages
+	----------------------------------------------------------------------
+	"""
 
-    def build(self, process, files, executableName, runtimeArgs):
-        # default arguments
-        args = ["-g", "-Wall", "-o", executableName]
-        
-        # concatenate default args & the list of files
+	def build(self, process, files, executableName, compileArgs):
 
-        # TODO: do we need to include a "customArgs" for runtime args that
-        #       a user might supply? (I think yes....)
+		# default arguments
+		executableName = os.path.dirname(str(files[0])) + "/" + executableName
+		print executableName
+		
+		args = ["-g", "-Wall", "-o", executableName]
 
-        #args.extend(runtimeArgs)
+		# concatenate default args & the list of files
 
-        args.extend(files)
+		# TODO: do we need to include a "customArgs" for runtime args that
+		#       a user might supply? (I think yes....)
 
-        # call the clang compiler with the specified args
-        process.start(QtCore.QString("clang++"), QtCore.QStringList(args))
+		#args.extend(runtimeArgs)
 
-    #
-    # Determines if a line is a clang error message
-    #
-    @staticmethod
-    def is_error_line(line):
-        if len(re.findall(".*:[0-9]+:[0-9]+: error:.*", line)) == 1:
-            return True
-        return False
+		args.extend(files)
 
-    #
-    # Determines if a line is a clang warning message
-    #
-    @staticmethod
-    def is_warning_line(line):
-        if len(re.findall(".*:[0-9]+:[0-9]+: warning:.*", line)) == 1:
-            return True
-        return False
+		# call the clang compiler with the specified argsf
+		process.start(QtCore.QString("clang++"), QtCore.QStringList(args))
 
-    #
-    # Parse a clang error/warning message and return a dictionary of information
-    # including: the file name, the line and character where the error was 
-    # recorded, the error type, and an error message. 
-    #
-    @staticmethod
-    def parse_clang_msg(line):
+	#
+	# Determines if a line is a clang error message
+	#
+	@staticmethod
+	def is_error_line(line):
+		if len(re.findall(".*:[0-9]+:[0-9]+: error:.*", line)) == 1:
+			return True
+		return False
 
-        # parse the various elements of a clang message
-        elements = line.split(":",4)
+	#
+	# Determines if a line is a clang warning message
+	#
+	@staticmethod
+	def is_warning_line(line):
+		if len(re.findall(".*:[0-9]+:[0-9]+: warning:.*", line)) == 1:
+			return True
+		return False
 
-        # capture the contents of a clang message w/ specific identifiers
-        retval = {}
-        retval['filename']   = elements[0]
-        retval['line_no']    = elements[1]
-        retval['char_no']    = elements[2]
-        retval['error_type'] = elements[3]
-        retval['error_msg']  = elements[4]
+	#
+	# Parse a clang error/warning message and return a dictionary of information
+	# including: the file name, the line and character where the error was 
+	# recorded, the error type, and an error message. 
+	#
+	@staticmethod
+	def parse_clang_msg(line):
 
-        return retval
+		# parse the various elements of a clang message
+		elements = line.split(":",4)
 
-    #
-    # Parse the clang compiler messages, returning a dictionary of the errors.
-    # [NOTE: could be empty]
-    #
-    @staticmethod
-    def parse_clang_output(self, output):
-        
-        # parse clang output line by line
-        lines = output.split("\n")
+		# capture the contents of a clang message w/ specific identifiers
+		retval = {}
+		retval['filename']   = elements[0]
+		retval['line_no']    = elements[1]
+		retval['char_no']    = elements[2]
+		retval['error_type'] = elements[3]
+		retval['error_msg']  = elements[4]
 
-        # determine all lines of output that are warnings/errors
-        sections = []
-        for i, line in enumerate(lines):
-            if self.is_error_line(line) or self.is_warning_line(line):
-                sections.append(i)
+		return retval
 
-        # for each warning/error message, get the 'full message' and append
-        # this data to a dictionary with other warning/error information. 
-        errors = []
-        for i, line_pos in enumerate(sections):
-            temp = self.parse_clang_msg(lines[line_pos])
+	#
+	# Parse the clang compiler messages, returning a dictionary of the errors.
+	# [NOTE: could be empty]
+	#
+	@staticmethod
+	def parse_clang_output(self, output):
 
-            if i + 1 < len(sections):
-                temp['full_msg'] = "\n".join(lines[line_pos:sections[i + 1]])
-            else:
-                temp['full_msg'] = "\n".join(lines[line_pos:])
+		# parse clang output line by line
+		lines = output.split("\n")
 
-            errors.append(temp)
+		# determine all lines of output that are warnings/errors
+		sections = []
+		for i, line in enumerate(lines):
+			if self.is_error_line(line) or self.is_warning_line(line):
+				sections.append(i)
 
-        return errors
+		# for each warning/error message, get the 'full message' and append
+		# this data to a dictionary with other warning/error information. 
+		errors = []
+		for i, line_pos in enumerate(sections):
+			temp = self.parse_clang_msg(lines[line_pos])
+
+			if i + 1 < len(sections):
+				temp['full_msg'] = "\n".join(lines[line_pos:sections[i + 1]])
+			else:
+				temp['full_msg'] = "\n".join(lines[line_pos:])
+
+			errors.append(temp)
+
+		return errors
 
