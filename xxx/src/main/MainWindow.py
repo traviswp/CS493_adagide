dccffe6b86d8b1d95e038663a2fd663328645d4c
Christopher Ryan Proteau <proteac@qt1>
2012-12-05 15:19:18 -0800

    makefile fix

--- xxx/src/main/MainWindow.py
+++ xxx/src/main/MainWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MainWindow.ui'
 #
-# Created: Tue Dec  4 11:48:26 2012
+# Created: Wed Dec  5 15:13:08 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -368,6 +368,7 @@ class Ui_MainWindow(object):
         self.actionStop.setText(QtGui.QApplication.translate("MainWindow", "Stop", None, QtGui.QApplication.UnicodeUTF8))
         self.actionStop.setShortcut(QtGui.QApplication.translate("MainWindow", "F7", None, QtGui.QApplication.UnicodeUTF8))
 
+#import cs140adagide_rc
 
 if __name__ == "__main__":
     import sys
