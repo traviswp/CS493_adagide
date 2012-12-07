5d22bd67a09abc3b340abad4a083fa38d2b95da1
traviswp <traviswp@gmail.com>
2012-12-07 10:16:37 -0800

    had to make my changes...

--- xxx/src/main/cs140adagide_rc.py
+++ xxx/src/main/cs140adagide_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Fri Dec 7 09:40:13 2012
+# Created: Fri Dec 7 10:16:09 2012
 #      by: The Resource Compiler for PyQt (Qt v4.7.2)
 #
 # WARNING! All changes made in this file will be lost!
